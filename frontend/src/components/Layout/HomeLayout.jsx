781968b044ead42b2d25527439247af843227a8a
Allen Tiempo <jatiempo89@gmail.com>
2024-07-26 22:44:59 +0800

    fixed the height of the home and login page

--- frontend/src/components/Layout/HomeLayout.jsx
+++ frontend/src/components/Layout/HomeLayout.jsx
@@ -5,22 +5,30 @@ import classes from "./HomeLayout.module.css";
 import SideNav from "../../components/UI/SideNav";
 import Footer from "../UI/Footer";
 
-
 const HomeLayout = () => {
   const user = useRouteLoaderData("home");
   const isAdmin = user && user.isadmin;
   const user_id = user && user.user_id;
+
   return (
     <>
       <MainNavigation user={user} />
-      <div className={classes.layout}>
-        {isAdmin && <SideNav user_id={user_id} className={classes.menu} />}
-        <main className={isAdmin ? classes.adminMain : classes.main}>
-          <Outlet />
-          {isAdmin && <Footer className={classes.footerAdmin} />}
-        </main>
-      </div>
-      {!isAdmin && <Footer className={classes.footer} />}
+      {isAdmin ? (
+        <div className={classes.layout}>
+          <SideNav user_id={user_id} className={classes.menu} />
+          <main className={classes.adminMain}>
+            <Outlet />
+            <Footer className={classes.footerAdmin} />
+          </main>
+        </div>
+      ) : (
+        <>
+          <main className={classes.main}>
+            <Outlet />
+          </main>
+          <Footer className={classes.footer} />
+        </>
+      )}
     </>
   );
 };
